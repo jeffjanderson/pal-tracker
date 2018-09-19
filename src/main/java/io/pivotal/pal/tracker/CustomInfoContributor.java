5d3c59f5223797fa47bbaea798c52c1d4a2b1f79
Jeff Anderson <jeff.anderson.lv3f@statefarm.com>
2018-09-19 08:06:54 -0600

    adds InfoContributor to get info from /info endpoint

---
+++ src/main/java/io/pivotal/pal/tracker/CustomInfoContributor.java
@@ -0,0 +1,23 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CustomInfoContributor implements InfoContributor {
+
+    @Override
+    public void contribute(Info.Builder builder) {
+        Map<String,Object> buildInfo = new HashMap<>();
+        buildInfo.put("org", "kent-miller");
+        buildInfo.put("space", "review");
+        buildInfo.put("author", "kent miller");
+
+        Map<String,Object> map = new HashMap<>();
+        map.put("buildInfo",buildInfo);
+
+        Info info = builder.build();
+    }
+}
